cab8d9ecec4f57bc9566b2ff3f7b83e9000c24b1
Ryan Sg <newruns@gmail.com>
2022-08-04 17:35:16 +0900

    Add a Tabs component

--- src/renderer/components/WebBrowser.jsx
+++ src/renderer/components/WebBrowser.jsx
@@ -1,14 +1,95 @@
+import React, { useRef, useState } from 'react';
+import { Tabs, Button, Popconfirm, Space, Upload } from 'antd';
+import WebView from './WebView';
+import AddressBar from './AddressBar';
+import './WebBrowser.css';
+
+const { TabPane } = Tabs;
+const initialPanes = [
+  {
+    title: 'Tab 1',
+    content: <WebView />,
+    key: '1',
+    closable: false,
+  },
+  {
+    title: 'Tab 2',
+    content: 'Content of Tab 2',
+    key: '2',
+  },
+  {
+    title: 'Tab 3',
+    content: 'Content of Tab 3',
+    key: '3',
+  },
+];
+
 export default function () {
+  const [activeKey, setActiveKey] = useState(initialPanes[0].key);
+  const [panes, setPanes] = useState(initialPanes);
+  const newTabIndex = useRef(0);
+
+  const onChange = (newActiveKey) => {
+    setActiveKey(newActiveKey);
+  };
+
+  const add = () => {
+    const newActiveKey = `newTab${newTabIndex.current++}`;
+    const newPanes = [...panes];
+    newPanes.push({
+      title: 'New Tab',
+      content: 'Content of new Tab',
+      key: newActiveKey,
+    });
+    setPanes(newPanes);
+    setActiveKey(newActiveKey);
+  };
+
+  const remove = (targetKey) => {
+    let newActiveKey = activeKey;
+    let lastIndex = -1;
+    panes.forEach((pane, i) => {
+      if (pane.key === targetKey) {
+        lastIndex = i - 1;
+      }
+    });
+    const newPanes = panes.filter((pane) => pane.key !== targetKey);
+
+    if (newPanes.length && newActiveKey === targetKey) {
+      if (lastIndex >= 0) {
+        newActiveKey = newPanes[lastIndex].key;
+      } else {
+        newActiveKey = newPanes[0].key;
+      }
+    }
+
+    setPanes(newPanes);
+    setActiveKey(newActiveKey);
+  };
+
+  const onEdit = (targetKey, action) => {
+    if (action === 'add') {
+      add();
+    } else {
+      remove(targetKey);
+    }
+  };
+
   return (
-    <div>
-      <webview
-        src="https://www.github.com/"
-        style={{
-          display: 'inline-flex',
-          width: '100%',
-          height: '100%',
-        }}
-      ></webview>
-    </div>
+    <Tabs
+      type="editable-card"
+      onChange={onChange}
+      activeKey={activeKey}
+      onEdit={onEdit}
+    >
+      {panes.map((pane) => (
+        <TabPane tab={pane.title} key={pane.key} closable={pane.closable}>
+          <AddressBar />
+          {pane.content}
+        </TabPane>
+      ))}
+    </Tabs>
   );
+
+  return <WebView />;
 }
---
+++ src/renderer/components/WebView.jsx
@@ -0,0 +1,12 @@
+export default function () {
+  return (
+    <webview
+      src="https://www.github.com/"
+      style={{
+        display: 'inline-flex',
+        width: '100%',
+        height: '100%',
+      }}
+    ></webview>
+  );
+}
