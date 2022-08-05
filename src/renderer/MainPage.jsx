22eda13c3c2eccaf765396793387652232d238ca
Ryan Sg <newruns@gmail.com>
2022-08-05 17:06:12 +0900

    Update a layout

--- src/renderer/MainPage.jsx
+++ src/renderer/MainPage.jsx
@@ -7,7 +7,7 @@ import './MainPage.css';
 export default function () {
   return (
     <FullPage>
-      <Split className="split" gutterSize={1} sizes={[70, 30]}>
+      <Split className="split" gutterSize={1} sizes={[60, 40]}>
         <WebBrowser></WebBrowser>
         <DevPanel></DevPanel>
       </Split>
