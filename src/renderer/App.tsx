6f340285e054f615d54c2182e49ee57966147e2f
Ryan Sg <newruns@gmail.com>
2022-08-05 10:59:02 +0900

    Enables Chakra-UI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,17 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import MainPage from './MainPage';
 import 'antd/dist/antd.css';
 import './App.css';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
