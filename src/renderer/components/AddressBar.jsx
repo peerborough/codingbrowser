060c4e16e6f094724f87c2192ca1371ea3da6852
Ryan Sg <newruns@gmail.com>
2022-08-05 10:59:14 +0900

    Update AddressBar.jsx

--- src/renderer/components/AddressBar.jsx
+++ src/renderer/components/AddressBar.jsx
@@ -1,3 +1,52 @@
+import {
+  HStack,
+  Box,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Input,
+  IconButton,
+} from '@chakra-ui/react';
+import { PhoneIcon } from '@chakra-ui/icons';
+import {
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  AiOutlineReload,
+  AiTwotoneLock,
+  AiOutlineSearch,
+} from 'react-icons/ai';
+
 export default function () {
-  return <div>Addressbar</div>;
+  return (
+    <HStack p="1" spacing="0">
+      <IconButton
+        size="sm"
+        variant="ghost"
+        colorScheme="gray"
+        aria-label="Go back"
+        icon={<AiOutlineArrowLeft />}
+      />
+      <IconButton
+        size="sm"
+        variant="ghost"
+        colorScheme="gray"
+        aria-label="Go forward"
+        icon={<AiOutlineArrowRight />}
+      />
+      <IconButton
+        size="sm"
+        variant="ghost"
+        colorScheme="gray"
+        aria-label="Reload this page"
+        icon={<AiOutlineReload />}
+      />
+      <InputGroup size="sm">
+        <InputLeftElement
+          pointerEvents="none"
+          children={<AiOutlineSearch color="grey" />}
+        />
+        <Input variant="outline" placeholder="Search or enter web address" />
+      </InputGroup>
+    </HStack>
+  );
 }
