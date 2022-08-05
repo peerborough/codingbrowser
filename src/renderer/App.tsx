c84c0f3bd2c89ba79d695c063b5261d12a2c9a93
Ryan Sg <newruns@gmail.com>
2022-08-05 16:59:07 +0900

    Initialize Monaco editor

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,6 +3,10 @@ import { ChakraProvider } from '@chakra-ui/react';
 import MainPage from './MainPage';
 import 'antd/dist/antd.css';
 import './App.css';
+import * as monaco from 'monaco-editor';
+import { loader } from '@monaco-editor/react';
+
+loader.config({ monaco });
 
 export default function App() {
   return (
