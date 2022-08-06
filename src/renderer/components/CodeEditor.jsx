ca8fb6a223dd82cd98f655c31d7f12e3e03abf30
Ryan Sg <newruns@gmail.com>
2022-08-07 00:50:15 +0900

    Add an imperative handle

--- src/renderer/components/CodeEditor.jsx
+++ src/renderer/components/CodeEditor.jsx
@@ -1,12 +1,28 @@
+import { forwardRef, useRef, useImperativeHandle } from 'react';
 import Editor from '@monaco-editor/react';
 
-export default function ({ defaultValue = '' }) {
+function CodeEditor({ defaultValue = '' }, ref) {
+  const editorRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    getValue: () => {
+      return editorRef.current?.getValue();
+    },
+  }));
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
   return (
     <Editor
       height="100%"
       defaultLanguage="javascript"
       defaultValue={defaultValue}
       options={{ minimap: { enabled: false } }}
+      onMount={handleEditorDidMount}
     />
   );
 }
+
+export default forwardRef(CodeEditor);
