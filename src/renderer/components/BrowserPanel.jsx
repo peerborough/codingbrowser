4ae07a5d6b2ecdb43eb951e28033ba9724cba3d3
Ryan Sg <newruns@gmail.com>
2022-08-08 18:24:46 +0900

    Implement tab title updating

--- src/renderer/components/BrowserPanel.jsx
+++ src/renderer/components/BrowserPanel.jsx
@@ -1,34 +1,49 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import WebBrowser from './WebBrowser';
 import { BrowserTabs, Dark, Light } from './BrowserTabs';
 
-const createTab = (tabId, url) => ({
-  title: 'New Tab ',
-  url: url,
-  id: tabId,
-  content: (props) => <WebBrowser tabId={tabId} url={url} />,
-});
-const defaultTabs = [createTab(nanoid(), 'https://github.com/')];
-
 export default function () {
-  const tabs = useState(defaultTabs);
-  const activeTab = useState(0);
+  const tabsState = useState([]);
+  const [tabs, setTabs] = tabsState;
+
+  const activeTabState = useState(0);
+  const [activeTab, setActiveTab] = activeTabState;
+
+  useEffect(() => {
+    addTab();
+  }, []);
+
+  const onUpdateTitle = (tabId, title) => {
+    setTabs((tabs) =>
+      tabs.map((tab) => (tab.id === tabId ? { ...tab, title: title } : tab))
+    );
+  };
 
   const addTab = () => {
     const newTabId = nanoid();
     const url = 'https://google.com/';
 
-    activeTab[1](tabs[0].length);
-    tabs[1]([...tabs[0], createTab(newTabId, url)]);
+    setActiveTab(tabs.length);
+    setTabs([...tabs, createTab(newTabId, url, onUpdateTitle)]);
   };
+
   return (
     <BrowserTabs
       onAddTabPress={addTab} // CallBack for a Tab Add
       theme={false ? Dark : Light} // Theming
       //      injectProps={{ isDark, setisDark }} // custom props that you needed it to be injected.
-      activeTab={activeTab} // keep a track of active index via state.
-      tabs={tabs} // tabs
+      activeTab={activeTabState} // keep a track of active index via state.
+      tabs={tabsState} // tabs
     />
   );
 }
+
+const createTab = (tabId, url, onTitleUpdated) => ({
+  title: 'New Tab ',
+  url: url,
+  id: tabId,
+  content: (props) => (
+    <WebBrowser tabId={tabId} url={url} onTitleUpdated={onTitleUpdated} />
+  ),
+});
