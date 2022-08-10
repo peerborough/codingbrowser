dd8f091f523c6da86307dd1bd06d63b67bdf6ca7
Ryan Sg <newruns@gmail.com>
2022-08-10 10:40:12 +0900

    Add a devtools pane

--- src/renderer/components/DevPanel.jsx
+++ src/renderer/components/DevPanel.jsx
@@ -1,5 +1,12 @@
+import { Allotment } from 'allotment';
 import CodePane from './CodePane';
+import DevToolsPane from './DevToolsPane';
 
 export default function () {
-  return <CodePane />;
+  return (
+    <Allotment defaultSizes={[300, 100]} vertical={true}>
+      <CodePane />
+      <DevToolsPane />
+    </Allotment>
+  );
 }
