ee8ecd5400ac2571e3421dd337b7bc4db40aa29b
Ryan Sg <newruns@gmail.com>
2022-08-22 08:53:26 +0900

    Update util.js

--- src/renderer/util.js
+++ src/renderer/util.js
@@ -1,3 +1,19 @@
 export const sleep = (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
+
+export const getAtJsPath = (input, obj) => {
+  return input.split('.').reduce((o, s) => o[s], obj);
+};
+
+export const setAtJsPath = (input, obj, val) => {
+  const pathValues = input.split('.');
+
+  pathValues.reduce((o, s, i) => {
+    if (i !== pathValues.length - 1) {
+      return o[s];
+    }
+
+    o[s] = val;
+  }, obj);
+};
