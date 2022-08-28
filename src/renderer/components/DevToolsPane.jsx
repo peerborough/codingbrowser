f05eaae6d61329753570fce8a322c2c1db5239de
Ryan Sg <newruns@gmail.com>
2022-08-28 14:33:03 +0900

    Remove the OutputMessages component from the DevToolsPane

--- src/renderer/components/DevToolsPane.jsx
+++ src/renderer/components/DevToolsPane.jsx
@@ -1,7 +1,6 @@
 import { VscChevronUp, VscClose } from 'react-icons/vsc';
 import { Tabs } from 'antd';
 import DebugConsole from './DebugConsole';
-import OutputMessages from './OutputMessages';
 import './DevToolsPane.css';
 
 const { TabPane } = Tabs;
@@ -10,10 +9,7 @@ export default function () {
   return (
     <div className="dev-tools-pane" style={{ height: '100%' }}>
       <Tabs defaultActiveKey="1" style={{ height: '100%' }}>
-        <TabPane tab="RUN" key="1">
-          <OutputMessages />
-        </TabPane>
-        <TabPane tab="DEBUG CONSOLE" key="2">
+        <TabPane tab="DEBUG CONSOLE" key="1">
           <DebugConsole />
         </TabPane>
       </Tabs>
