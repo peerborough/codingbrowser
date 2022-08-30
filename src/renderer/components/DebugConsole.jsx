210e57b69382647922ccc7ede3b2921be3955080
Ryan Sg <newruns@gmail.com>
2022-08-31 08:42:53 +0900

    Scroll to bottom automatically

--- src/renderer/components/DebugConsole.jsx
+++ src/renderer/components/DebugConsole.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useRef } from 'react';
 import { Console } from 'console-feed';
 import useConsoleLog from '../workspace/useConsoleLog';
 
 export default function DebugConsole({ defaultScript }, ref) {
   const { consoleLogs } = useConsoleLog();
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(scrollToBottom, [consoleLogs]);
 
   return (
     <div style={{ height: '100%' }}>
       <div style={{ height: '100%', overflow: 'auto' }}>
         <Console logs={consoleLogs}></Console>
+        <div ref={messagesEndRef} />
       </div>
     </div>
   );
